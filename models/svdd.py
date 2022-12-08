cb50fa4b1b7afe399cd1af09b1ea3843b1a5af86
LanpingTech <lanping_tech@qq.com>
2022-12-08 15:59:57 +0800

    supervised version

--- models/svdd.py
+++ models/svdd.py
@@ -9,7 +9,7 @@ class SVDD(nn.Module):
         self.pdist = nn.PairwiseDistance(p=2)
 
     def forward(self, x):
-        return  torch.sigmoid(self.pdist(x, self.C) - self.R)
+        return torch.sigmoid(self.pdist(x, self.C) - self.R)
 
 class SVDD_Plus(nn.Module):
     def __init__(self, dim):
@@ -19,4 +19,22 @@ class SVDD_Plus(nn.Module):
         self.pdist = nn.PairwiseDistance(p=2)
 
     def forward(self, x):
-        return  torch.sigmoid(self.pdist(x, self.C) - self.R).view(-1)
+        return torch.sigmoid(self.pdist(x, self.C) - self.R).view(-1)
+
+class BCEFocalLoss(torch.nn.Module):
+    def __init__(self, gamma=2, alpha=0.25, reduction='mean'):
+        super(BCEFocalLoss, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.reduction = reduction
+
+    def forward(self, predict, target):
+        pt = predict # sigmoide获取概率
+        #在原始ce上增加动态权重因子，注意alpha的写法，下面多类时不能这样使用
+        loss = - self.alpha * (1 - pt) ** self.gamma * target * torch.log(pt) - (1 - self.alpha) * pt ** self.gamma * (1 - target) * torch.log(1 - pt)
+
+        if self.reduction == 'mean':
+            loss = torch.mean(loss)
+        elif self.reduction == 'sum':
+            loss = torch.sum(loss)
+        return loss
--- t.py
+++
@@ -1,6 +0,0 @@
-import torch
-import torch.nn as nn
-
-x = torch.ones(2, 4)
-y = torch.ones(1, 4) *3
-print(x - y)
